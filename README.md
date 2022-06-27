69b44b2ca143ef92c3cd76eabd83d75949c58291
Júlia Cordeiro <julia.c2001@hotmail.com>
2022-06-26 21:54:33 -0300

    Implement Cut leaf mass center

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # plantation_analysis-PDI
 This project is destined to analyses phosphate levels in potato leaves.
+
+## Authors:
+``` 
+André Andrade dos Santos
+Júlia Cordeiro e Silva
+Murilo Toledo
+``` 
--- main.py
+++ main.py
@@ -1,24 +1,11 @@
 from matplotlib import pyplot as plt
 from turtle import color
+from datetime import datetime
 import cv2
 import numpy as np
 import os, sys
 
-#Função de Convolução 2D
-def convolution2d(imagem, kernel):
-    m, n = kernel.shape
-    c = m % 2 
-    #garantir que o kernel é impar e do quadrado
-    if(m == n) & (c != 0):
-        y, x = imagem.shape
-        nova_imagem = np.zeros((y,x))
-        #ignorar os pontos de borda que extrapolariam
-        y = y - m + 1
-        x = x - m + 1
-        for i in range(y):
-            for j in range(x):
-                nova_imagem[i+c][j+c] = np.sum(imagem[i:i+m, j:j+m]*kernel)
-    return nova_imagem
+print(datetime.now())
 
 def resize_image(image):
     scale_percent = 10
@@ -29,9 +16,11 @@ def resize_image(image):
     return image_resize
 
 
-path = "./test"
+path = "./LuzBranca_Cima"
 for filename in os.listdir(path):
     if filename.endswith('.jpg'):
+        imagename = filename[:-4]
+
         img = cv2.imread(path + "/" + filename)
         # cv2.imshow('imagem', resize_image(img))
 
@@ -59,9 +48,9 @@ for filename in os.listdir(path):
 
         nova = cv2.merge([img_B, img_G, img_R])
         # cv2.imshow('imagem nova', resize_image(nova))
-        cv2.imwrite(f"imagens_auxiliares/${filename}.jpg", nova)
+        cv2.imwrite(f"imagens_auxiliares/{imagename}.jpg", nova)
 
-        nova = cv2.imread(f"imagens_auxiliares/${filename}.jpg")
+        nova = cv2.imread(f"imagens_auxiliares/{imagename}.jpg")
         # cv2.imshow('imagem nova', resize_image(nova))
 
         img_hsvnova = cv2.cvtColor(nova, cv2.COLOR_BGR2HSV)
@@ -80,7 +69,7 @@ for filename in os.listdir(path):
         # apply Otsu's automatic thresholding which automatically determines
         # the best threshold value
         (T, binI) = cv2.threshold(suave, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)
-        print("[INFO] otsu's thresholding value: {}".format(T))
+        # print("[INFO] otsu's thresholding value: {}".format(T))
 
         #Aplicação de MORFOLOGIA
         kernel = np.ones((75, 75), np.uint8) 
@@ -116,16 +105,67 @@ for filename in os.listdir(path):
         # apply Otsu's automatic thresholding which automatically determines
         # the best threshold value
         (T, binIg) = cv2.threshold(resg, 100, 255, cv2.THRESH_BINARY)
-        print("[INFO] otsu's thresholding value: {}".format(T))
+        # print("[INFO] otsu's thresholding value: {}".format(T))
 
         binIgsuave = cv2.medianBlur(binIg, 11)
         # cv2.imshow('imagem binIg Suave', resize_image(binIgsuave))
 
+        cv2.imwrite(f"folha_extraida/{imagename}.jpg", resultado2) 
+
 ''' End of extract leaf from image'''
 
+pathMass = "./folha_extraida"
+for filename in os.listdir(pathMass):
+    if filename.endswith('.jpg'):
+        imagename = filename[:-4]
 
-        # cv2.imwrite(f"folha_extraida/${filename}.jpg", ) 
+        original = cv2.imread(path + "/" + filename)
+        img = cv2.imread(pathMass + "/" + filename, cv2.IMREAD_GRAYSCALE)
+        # cv2.imshow('imagem', resize_image(img))
+
+        (T, binIm) = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY)
+        # print("[INFO] otsu's thresholding value: {}".format(T))
+        # print(f"Thresh: {binIm}")
         
+        height, width = binIm.shape[:2]
+        # print(f"H: {height}, W: {width}")
 
+        mass = 0
+        x_size = 0.0
+        y_size = 0.0
+
+        for i in range(width):
+            for j in range(height):
+                if not binIm[j][i]:
+                    mass += 1
+                    x_size += i
+                    y_size += j
+        # print(f"mass: {mass}, X: {x_size}, Y: {y_size}")
+
+        x_size = x_size/mass
+        y_size = y_size/mass
+        # print(f"X: {x_size}, Y: {y_size}")
+
+        x = int(x_size)
+        y = int(y_size)
+        # print(f"X: {x}, Y: {y}")
+
+        cut = original[(y-100):(y+100), (x-100):(x+100)]
+        # cv2.imshow("Recorte da imagem", cut)
+        cv2.imwrite(f"image_first_cut/{imagename}.jpg", cut) 
+
+        width_orig = original.shape[1]
+        height_orig = original.shape[0]
+
+        y_center = int(height_orig // 2)
+        x_center = int(width_orig // 2)
+
+        cut_2 = original[(y_center-100):(y_center+100), (x_center-100):(x_center+100)]
+        # cv2.imshow("Recorte 2 da imagem", cut_2)
+        cv2.imwrite(f"image_second_cut/{imagename}.jpg", cut_2)
+
+''' End of cut image mass center'''
+
+print(datetime.now())
 #espera para fechar programa
 cv2.waitKey(0) #espera pressionar qualquer tecla
